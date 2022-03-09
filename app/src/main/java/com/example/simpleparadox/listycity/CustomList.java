76e5bb9d4eda643ccd832887d77862f92bed897f
Rebecca Walton <grwalton@ualberta.ca>
2022-03-08 17:25:08 -0700

    Add test

--- app/src/main/java/com/example/simpleparadox/listycity/CustomList.java
+++ app/src/main/java/com/example/simpleparadox/listycity/CustomList.java
@@ -25,6 +25,13 @@ public class CustomList extends ArrayAdapter<City> {
         this.context = context;
     }
 
+    public int getCount() {
+        return cities.size();
+    }
+
+    public void addCity(City city) {
+        return;
+    }
 
     @NonNull
     @Override
